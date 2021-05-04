bc8caf35998162bca8df49a64646464ae03273a5
@SkyHook <mac@macs-MacBook-Pro.local>
2021-05-04 11:23:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+ 
+session_start();
+ 
+if(isset($_GET['logout'])){    
+     
+    //Simple exit message
+    $logout_message = "<div class='msgln'><span class='left-info'> <b class='user-name-left'>". $_SESSION['name'] ."</b>  left the Room.</span><br></div>";
+    file_put_contents("log.html", $logout_message, FILE_APPEND | LOCK_EX);
+     
+    session_destroy();
+    header("Location: index.php"); //Redirect the user
+}
+ 
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please  choose a user name</span>';
+    }
+}
+ 
+function loginForm(){
+    echo
+    '<div id="loginform">
+    <form action="index.php" method="post">
+      <label for="name">your user name</label>
+      <input type="text" name="name" id="name" />
+      <input type="submit" name="enter" id="enter" value="Get In!"/>
+    </form>
+  </div>';
+}
+ 
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+ 
+        <title>@Chat 2021</title>
+        <meta name="description" content="Tuts+ Chat Application" />
+        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+    </head>
+    <body>
+    <?php
+    if(!isset($_SESSION['name'])){
+        loginForm();
+    }
+    else {
+    ?>
+        <div id="wrapper">
+            <div id="menu">
+                <p class="welcome">Hello, <b><?php echo $_SESSION['name']; ?></b></p>
+                <p class="logout"><a id="exit" href="#">Leave Room</a></p>
+            </div>
+ 
+            <div id="chatbox">
+            <?php
+            if(file_exists("log.html") && filesize("log.html") > 0){
+                $contents = file_get_contents("log.html");          
+                echo $contents;
+            }
+            ?>
+            </div>
+ 
+            <form name="message" action="">
+                <input name="usermsg" type="text" id="usermsg" />
+                <input name="submitmsg" type="submit" id="submitmsg" value="send!" />
+            </form>
+        </div>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script type="text/javascript">
+            // jQuery Document
+            $(document).ready(function () {
+                $("#submitmsg").click(function () {
+                    var clientmsg = $("#usermsg").val();
+                    $.post("post.php", { text: clientmsg });
+                    $("#usermsg").val("");
+                    return false;
+                });
+ 
+                function loadLog() {
+                    var oldscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height before the request
+ 
+                    $.ajax({
+                        url: "log.html",
+                        cache: false,
+                        success: function (html) {
+                            $("#chatbox").html(html); //Insert chat log into the #chatbox div
+ 
+                            //Auto-scroll           
+                            var newscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height after the request
+                            if(newscrollHeight > oldscrollHeight){
+                                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+                            }   
+                        }
+                    });
+                }
+ 
+                setInterval (loadLog, 2500);
+ 
+                $("#exit").click(function () {
+                    var exit = confirm("Are you sure you want to leave the room?");
+                    if (exit == true) {
+                    window.location = "index.php?logout=true";
+                    }
+                });
+            });
+        </script>
+
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.js"></script>
+    </body>
+</html>
+<?php
+}
+?>
